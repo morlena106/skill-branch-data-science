148e9195dd565e2952af6b142fa4d2b3e5d8336c
morlena106 <62900649+morlena106@users.noreply.github.com>
2021-01-17 23:37:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,3 +21,23 @@ def gradient_optimization_one_dim(func):
         pr = (func(x + e) - func(x)) / e
         x = x - e*pr
     return round(x, 2)
+
+def gradient_optimization_multi_dim(func):
+    e = 0.001
+    x1 = 4
+    x2 = 10
+    
+    for i in range(50):
+
+        x = [x1, x2]
+        x_1 = [x1 + e, x2]
+        x_2 = [x1, x2 + e]
+
+        pr1 = (func(x_1) - func(x)) / e
+        pr2 = (func(x_2) - func(x)) / e
+
+        x1 = x1 - e * pr1
+        x2 = x2 - e * pr2
+        
+    x = [round(x1, 2), round(x2, 2)]
+    return x
