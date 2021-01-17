eb4b192ae5c91359bbb694c457f84056eb099f2b
morlena106 <62900649+morlena106@users.noreply.github.com>
2021-01-17 22:47:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,18 @@ def gradient(xlist, func):
     e = 0.0001
     x1 = xlist[0]
     x2 = xlist[1]
-    pr1 = (func(x1 + e, x2) - func(x1, x2)) / e
-    pr2 = (func(x1, x2 + e) - func(x1, x2)) / e
+    pr1 = (func([x1 + e, x2]) - func([x1, x2])) / e
+    pr2 = (func([x1, x2 + e]) - func([x1, x2])) / e
     grad = [round(pr1,2),round(pr2,2)]
     return grad
+    
+def gradient_optimization_one_dim(func):
+    e = 0.001
+    x = 10
+    for i in range(50):
+        pr = (func(x + e) - func(x)) / e
+        if pr > 0:
+            x -= e
+        else:
+            x += e
+    return x 
