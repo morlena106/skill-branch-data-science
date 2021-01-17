203a3b89186dbcc86ef00a451e122f6d788bddaa
morlena106 <62900649+morlena106@users.noreply.github.com>
2021-01-17 22:37:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ def derivation(x,func):
     e = 0.0001
     pr = (func(x + e) - func(x)) / e
     return round(pr,2)
+
+def gradient(xlist, func):
+    e = 0.0001
+    x1 = xlist[0]
+    x2 = xlist[1]
+    pr1 = (func(x1 + e, x2) - func(x1, x2)) / e
+    pr2 = (func(x1, x2 + e) - func(x1, x2)) / e
+    grad = [round(pr1,2),round(pr2,2)]
+    return grad
