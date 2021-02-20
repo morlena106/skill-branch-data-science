1d0fe98bde55429539ed21f4c38448f402364391
morlena106 <62900649+morlena106@users.noreply.github.com>
2021-02-21 01:13:48 +0300

    Update task_3_solution.py

--- task_3_solution.py
+++ task_3_solution.py
@@ -1,7 +1,14 @@
 import pandas as pd 
+import numpy as np
 
 def split_data_into_two_samples(data):
     
     train_data, test_data = train_test_split(data, test_size=0.3, random_state=42)
     
     return train_data, test_data
+
+
+def prepare_data(data):
+    #select_data = data.select_dtypes(exclude=[object])
+    select_data = data.select_dtypes([np.number])
+    return select_data
