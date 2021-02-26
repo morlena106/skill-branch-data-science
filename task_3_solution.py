c950eadcc666528dbffc38bc1cd0947a25ad81f2
morlena106 <62900649+morlena106@users.noreply.github.com>
2021-02-27 00:02:03 +0300

    Update task_3_solution.py

--- task_3_solution.py
+++ task_3_solution.py
@@ -1,5 +1,19 @@
-import pandas as pd 
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from copy import deepcopy
+from tqdm import tqdm
+
+from sklearn.datasets import make_regression
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.model_selection import train_test_split
+%matplotlib inline
 
 def split_data_into_two_samples(data):
     
