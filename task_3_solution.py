f70218d03206daec6167c48357b6ec1a15f13402
morlena106 <62900649+morlena106@users.noreply.github.com>
2021-02-28 21:50:40 +0300

    Update task_3_solution.py

--- task_3_solution.py
+++ task_3_solution.py
@@ -9,13 +9,14 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import train_test_split
 
+#1
 def split_data_into_two_samples(data):
     
     train_data, test_data = train_test_split(data, test_size=0.3, random_state=42)
     
     return train_data, test_data
 
-
+#2
 def prepare_data(data):
     #select_data = data.select_dtypes(exclude=[object])
     data = data.drop(columns=["id"])
@@ -24,26 +25,33 @@ def prepare_data(data):
     select_data = data.select_dtypes([np.number])
     return select_data.dropna(axis=1), y
 
+#3
 def scale_data(data, scaler):
     numeric_data = data.select_dtypes([np.number])
     numeric_features = numeric_data.columns
     data_scaler = scaler.fit_transform(data[numeric_features])
     return data_scaler
 
+#4
 def prepare_data_for_model(data,scaler):
     
-    data = data.drop(columns=["Id"])
-    y = data["SalePrice"]
-    data = data.drop(columns=["SalePrice"])
+    data = data.drop(columns=["id"])
+    y = data["price_doc"]
+    data = data.drop(columns=["price_doc"])
     
     numeric_data = data.select_dtypes([np.number])
+    
+    numeric_data = numeric_data.dropna(axis=1)
+    
     numeric_features = numeric_data.columns
-    data_scaler = scaler.fit_transform(data[numeric_features])
+    
+    data_scaler = scaler.fit_transform(numeric_data[numeric_features])
     
     return data_scaler, y 
 
-from sklearn.linear_model import LinearRegression
 
+from sklearn.linear_model import LinearRegression
+#5
 def fit_first_linear_model(x_train,y_train):
     model = LinearRegression()
     my_model = model.fit(x_train, y_train)
