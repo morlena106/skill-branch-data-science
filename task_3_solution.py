0a2b6cc572a896e3ba4ec2fa289f7d7534e45904
morlena106 <62900649+morlena106@users.noreply.github.com>
2021-03-06 00:59:56 +0300

    Update task_3_solution.py

--- task_3_solution.py
+++ task_3_solution.py
@@ -75,7 +75,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 def evaluate_model(model, x_valid, y_valid):  # 7
 
     y_pred = model.predict(x_valid)
-    mse = round(mean_squared_error(y_valid, y_pred), 2)
+    mse = round(np.sqrt(mean_squared_error(y_valid, y_pred)), 2)
     mae = round(mean_absolute_error(y_valid, y_pred), 2)
     r2 = round(r2_score(y_valid, y_pred), 2)
     return mse, mae, r2
